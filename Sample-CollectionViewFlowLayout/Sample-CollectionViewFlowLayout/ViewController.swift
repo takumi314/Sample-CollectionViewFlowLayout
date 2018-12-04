d08b5003640c4dd19bec5757b252eef0364aaea5
takumi314 <isospin_symmetry@me.com>
2018-12-05 01:56:46 +0900

    add initial project

---
+++ Sample-CollectionViewFlowLayout/Sample-CollectionViewFlowLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample-CollectionViewFlowLayout
+//
+//  Created by NishiokaKohei on 05/12/2018.
+//  Copyright © 2018 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
